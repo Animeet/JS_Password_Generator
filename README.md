6ce96d4d7d7cd093c6723812d032bdf29f4fa31e
Zachary Barnes <zachhsavage@gmail.com>
2023-03-12 14:32:56 -0400

    README update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# JS_Password_Generator
+# <JS_Password_Generator>
+
+## Description
+
+I worked around some ways to add proper function to this Password Generator.
+With this Generator, I was able to create a prompt, and a few confirm boxes to add some styling.
+Uniquely, this should prompt a new password everytime.
+
+
+Deployed Application URL: https://animeet.github.io/JS_Password_Generator/
+
+## Installation
+
+N/A
+
+## Usage
+
+You'll be able to use this generator to really add up some exclusivity and privacy to your passwords by mixing in numbers, characters, lower case letters, and capital letters!
+Keep in mind, you can generate a password between 8 and 128 characters in length. Meaning, if you plan to make a very long password, remember to save, write down, or memorize your unique password!
+
+## Credits
+
+Paul Keldsen was able to audibly get my brain thinking on the right track for where to start - https://www.youtube.com/watch?v=x4HUaiazDes
+
+JD Tadlock (Instructor) on providing some additional videos that may help with this project. - https://gist.github.com/jdtdesigns/9cfe05400063902e46aa5f6faabff0ca
+
+## License
+
+Please refer to the LICENSE in the repository.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 var passwordLength;
 var lowerCaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var upperCaseCharacter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var numberCharacter = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var numberCharacter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '|', '?', '<', '>']
 
 var generateBtn = document.querySelector("#generate");
@@ -14,44 +14,42 @@ var generateBtn = document.querySelector("#generate");
 
 ////  PROMPTS THE USER NEEDS TO ANSWER BEFORE GENERATING A PASSWORD  ////
 
-function openPrompt(){
-  choiceArray = [];
+function promptAnswers(){
+  passArray = [];
   passwordLength = parseInt(prompt('How long do you want your password? (8 - 128 characters)'));
 
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert ('Password length has to be a number between, 8 - 128 digits. Please try again.');
     return false;
   }
 
   if (confirm ("Would you like lowercase letters?")) {
-    choiceArray = choiceArray.concat(lowerCaseCharacter);
+    passArray = passArray.concat(lowerCaseCharacter);
   }
 
   if (confirm ("Would you like uppercase letters?")) {
-    choiceArray = choiceArray.concat(upperCaseCharacter);
+    passArray = passArray.concat(upperCaseCharacter);
   }
 
   if (confirm ("Would you like numbers?")) {
-    choiceArray = choiceArray.concat(numberCharacter);
+    passArray = passArray.concat(numberCharacter);
   }
 
   if (confirm ("Would you like special characters?")) {
-    choiceArray = choiceArray.concat(specialCharacter);
+    passArray = passArray.concat(specialCharacter);
   }
   return true;
 }
 
 
 
-
 //// ONCE PROMPTS ARE CORRECT////
 
 function writePassword() {
-  var correctPrompt = openPrompt();
+  var correctAnswers = promptAnswers();
   var passwordText = document.querySelector("#password");
 
 
-  if (correctPrompt) {
+  if (correctAnswers) {
     var newPassword = generatePassword();
     passwordText.value = newPassword;
   }
@@ -63,8 +61,8 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword () {
   var password = '';
   for(i = 0; i < passwordLength; i++){
-    var randomIndex = Math.floor(Math.random() * choiceArray.length);
-    password = password + choiceArray[randomIndex];
+    var randomIndex = Math.floor(Math.random() * passArray.length);
+    password = password + passArray[randomIndex];
   }
   return password;
 }
